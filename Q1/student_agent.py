319556f7f0b33a7572ed4238560153aa8d788401
jameslee <b10902111@ntu.edu.tw>
2025-05-04 10:54:42 +0800

    upload trained agent
    - Torch traced model
    - Only need actor
    - Score > -300

--- Q1/student_agent.py
+++ Q1/student_agent.py
@@ -1,13 +1,22 @@
-import gymnasium as gym
 import numpy as np
+import torch
 
-# Do not modify the input of the 'act' function and the '__init__' function. 
+device = "cpu"
+
+
+# Do not modify the input of the 'act' function and the '__init__' function.
 class Agent(object):
     """Agent that acts randomly."""
+
     def __init__(self):
         # Pendulum-v1 has a Box action space with shape (1,)
         # Actions are in the range [-2.0, 2.0]
-        self.action_space = gym.spaces.Box(-2.0, 2.0, (1,), np.float32)
+        self.actor = torch.jit.load(f"actor.pt", map_location=device)
+        self.actor.eval()
 
     def act(self, observation):
-        return self.action_space.sample()
+        observation = torch.tensor(
+            np.array([observation]), dtype=torch.float32, device=device
+        )
+        _, _, deterministic_action = self.actor(observation)
+        return deterministic_action.detach().cpu().numpy().squeeze(1)
